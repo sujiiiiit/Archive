5ee5dc98869b8caf4b388e6abfd6fb2fe431650c
Sujit Dwivedi <dwiwedisv@rknec.edu>
2024-05-26 13:50:23 +0530

    add custom-[font,colors,screen,animations,keyframes]

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   darkMode: "class",
   theme: {
+    //custom-fonts
     fontFamily: {
       Inter: ['"Inter"', "sans-serif"],
       SyoogBold: ['"SyoogBold"', "sans-serif"],
@@ -10,20 +11,24 @@ export default {
       GeistMono: ['"GeistMono"', "sans-serif"],
     },
     extend: {
+      //custom colors
       colors: {
         textPrimary: "var(--text-primary)",
         textSecondary: "var(--text-secondary)",
         transparentWhite: "var(--graph-fill)",
       },
+      //custom screen sizes
       screens: {
         xs: {
           raw: "(max-width: 580px)",
         },
       },
+      //custom animation
       animation: {
         shimmer: "shimmer 8s infinite",
         "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
       },
+      //custom keyframes
       keyframes: {
         shimmer: {
           "0%, 90%, 100%": {
